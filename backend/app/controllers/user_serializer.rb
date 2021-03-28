19ad6ab55e96984fde54c14705e6d145fa994137
jessie <jssdnvn@gmail.com>
2021-03-28 12:53:32 -0500

    added user serializer, routes and cors

---
+++ backend/app/controllers/user_serializer.rb
@@ -0,0 +1,4 @@
+class UserSerializer < AcriveModel::UserSerializer  
+    attributes :id, :name, :email, :trips
+end
+
--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,12 +5,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+ Rails.application.config.middleware.insert_before 0, Rack::Cors do
+   allow do
+     origins '*'
+
+     resource '*',
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+   end
+ end
+
+#methods (string or array): The HTTP methods allowed for the resource.
+#headers (string or array or :any): The HTTP headers that will be allowed in the CORS resource request. 
+    #Use :any to allow for any headers in the actual request.
