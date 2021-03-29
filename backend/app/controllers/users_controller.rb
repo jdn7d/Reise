fa02783f8b7b5db5b88364820395ae37b2de5415
jessie <jssdnvn@gmail.com>
2021-03-28 22:52:58 -0500

    added index and show methods in user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    
+    def index 
+        @users = User.all 
+        render json: @users
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render json: @user
+    end
+    
+    def create
+        
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
 end
