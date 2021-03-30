a6ff31a86fa8497659ee0d4129b80f9a0db90906
jessie <jssdnvn@gmail.com>
2021-03-30 13:25:05 -0500

    added encode and decode token mehthods

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode payload, ENV['JWT_SECRET']
+    end
+
+    def decoded_token
+        if auth_header
+          token = auth_header.split(' ')[1]
+          begin
+            JWT.decode(token, ENV['JWT_SECRET'])[0]
+          rescue JWT::DecodeError
+            nil
+          end
+        end
+      end
+
+    def auth_header 
+        request.headers['Authorization']
+    end
+    
+
+
 end
