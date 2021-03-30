70deb9f2901f7983f9c41bf5085609bb788fba36
jessie <jssdnvn@gmail.com>
2021-03-30 17:07:17 -0500

    added home component, and path in app.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Home extends React.Component{
+
+    render() {
+        return (
+            <div id="home">
+                <hi>Welcome</hi>
+            </div>
+        )
+    }
+}
+
+export default Home
